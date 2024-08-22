6fa7207a96c08644bb618e578f845076f8becdda
quyenhoang140299 <hoangquyen140299@gmail.com>
2024-08-22 14:08:33 +0700

    fetch favorite

--- utils/actions.ts
+++ utils/actions.ts
@@ -249,3 +249,27 @@ export const toggleFavoriteAction = async (prevState: {
         return renderError(error);
     }
 };
+
+// ### fetchFavorites
+export const fetchFavorites = async () => {
+    const user = await getAuthUser();
+    const favorites = await db.favorite.findMany({
+        where: {
+            profileId: user.id,
+        },
+        select: {
+            property: {
+                select: {
+                    id: true,
+                    name: true,
+                    tagline: true,
+                    price: true,
+                    country: true,
+                    image: true,
+                },
+            },
+        },
+    });
+
+    return favorites.map((favorite) => favorite.property);
+};
